762de5432da913331065714066e4d0d1e459350d
Jan Pivoňka <jan.pivonka.job@gmail.com>
2026-02-03 14:34:17 +0100

    edit next config for test online upload

--- p3b-core/next.config.ts
+++ p3b-core/next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Vercel ignoruje chyby v TypeScriptu a build dokončí
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Vercel ignoruje varování ESLintu
+    ignoreDuringBuilds: true,
+  },
+  // Pokud používáš Three.js/R3F, tohle může pomoci s optimalizací
+  transpilePackages: ['three'],
 };
 
 export default nextConfig;
